48a81499c118b4b2759f5f69f3ab6bf64ef813f0
dl222es <dl222es@student.lnu.se>
2015-08-31 21:49:40 +0200

    fixed date

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -4,8 +4,26 @@ class DateTimeView {
 
 
 	public function show() {
-
-		$timeString = 'TODO, Write servertime here...';
+            $day = date("l");       // Day in the week in letters
+            $number = date("jS");   // The day in number with "st" after
+            $month = date("F");     // Month in letters
+            $year = date("Y");      // Year in letters
+            $time = date("h:i:s");  // The time
+            
+            $hours = intval(date("h"));
+            $minutes = date("i");
+            $seconds = date("s");
+            $midday = date("A");
+            // If it is PM, add 12 hours to get digital time
+            if($midday == "PM")
+            {
+                $hours += 12; 
+            }
+            $time = $hours.":".$minutes.":".$seconds;
+            
+		$timeString = $day.", the ".$number." of ".$month." ".$year.
+                        ", The time is ".$time;
+            
 
 		return '<p>' . $timeString . '</p>';
 	}
